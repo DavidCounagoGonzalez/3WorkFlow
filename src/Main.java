a28645ed37a0478bebdf72d05517f32583a9b321
Damian Nogueiras <damian@danielcastelao.org>
2022-01-12 14:24:53 +0100

    Nombre Damian

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Damian");
+
         System.out.println("Hola, buenas ");
         Hora obxHora = new Hora();
         obxHora.MostrarHora();
@@ -7,5 +9,7 @@ public class Main {
         obxTiempo.mostraTiempo();
 
         System.out.println("David");
+
+
     }
 }
