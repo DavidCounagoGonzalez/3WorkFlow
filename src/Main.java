1f99f9e3780cfbe13fd9572e98c5390039d31c32
Nuria Garcia Cobas <ngarciacobas@danielcastelao.org>
2022-01-12 14:02:51 +0100

    Subido prueba main, hora y tiempo

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, buenas");
-        Hora obxHora= new Hora();
+        Hora obxHora = new Hora();
         obxHora.MostrarHora();
+        Tiempo obxTiempo = new Tiempo();
+        obxTiempo.mostraTiempo();
     }
 }
